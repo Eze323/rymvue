529ffd031ca841190fe63be61ec5d202ef43f1c1
Eze323 <a.Ezequielirace@gmail.com>
2023-04-03 05:59:11 -0300

    onsearch

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<script >
+import {ref,watchEffect} from 'vue';
+const API_URL='https://rickandmortyapi.com/api/character/';
+
+const commits=ref(null);
+
+
+watchEffect(async ()=>{
+
+    const url=`${API_URL}`;
+    commits.value=await (await fetch(url)).json();
+})
+
+
+</script>
+<template>
+    <h1>Lista de personajes</h1>
+
+        
+        <ul >
+            <li v-for="{name, image,gender} in commits">
+            <span>{{commit.name}}</span>
+            <span>{{commit.gender}}</span>
+            <img src={{commit.image}}/>
+
+            </li>
+        </ul>
+    
+</template>
